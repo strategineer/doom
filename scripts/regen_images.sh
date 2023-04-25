7c946f79062bc4e0218848af52e3ace775063f6c
Bill Tyros <me@strategineer.com>
2023-04-25 16:19:05 -0400

    bob

---
+++ scripts/regen_images.sh
@@ -0,0 +1,7 @@
+
+cd wads/castle_of_doom/img
+../../../scripts/drawmaps2.py ../castle_of_doom.wad MAP01 5000
+cd ../../../wads/speed001/img
+../../../scripts/drawmaps2.py ../speed001.wad MAP01
+cd ../../../wads/speed002/img
+../../../scripts/drawmaps2.py ../speed002.wad MAP01
