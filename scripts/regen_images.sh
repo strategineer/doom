d96a5402f9f887a94ede3872e74206ea8ddb0eb0
Bill Tyros <me@strategineer.com>
2023-04-25 23:51:46 -0400

    blah

--- scripts/regen_images.sh
+++ scripts/regen_images.sh
@@ -1,7 +1,9 @@
-
+cd /mnt/c/Users/Bill\ Tyros/Documents/GitHub/doom
 cd wads/castle_of_doom/img
 ../../../scripts/drawmaps2.py ../castle_of_doom.wad MAP01 5000
 cd ../../../wads/speed001/img
 ../../../scripts/drawmaps2.py ../speed001.wad MAP01
 cd ../../../wads/speed002/img
 ../../../scripts/drawmaps2.py ../speed002.wad MAP01
+cd ../../../wads/colosseum/img
+../../../scripts/drawmaps2.py ../MAP01.wad MAP01
