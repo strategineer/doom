e807d44675f56e4b5f626862f521c3affcadacf2
Bill Tyros <me@strategineer.com>
2023-05-01 19:18:09 -0400

    Fix build system

--- wads/tomb_of_doom/build.sh
+++ wads/tomb_of_doom/build.sh
@@ -1,1 +1,2 @@
-wtexscan tomb_of_doom.wad |  wtexport texpack/32in24-15_tex_v2.wad --base-wad /mnt/c/games/doom/iwads/doom2.wad -o tomb_of_doom.wad --add
+wtexscan MAP01.wad | wtexport texpack/32in24-15_tex_v2.wad --base-wad /mnt/c/games/doom/iwads/doom2.wad -o MAP01_tex.wad --create
+wadmerge build.txt
