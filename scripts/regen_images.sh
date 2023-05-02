81ca506e30c7d545e06384beafaa8ef2d503faba
Bill Tyros <me@strategineer.com>
2023-05-01 21:47:18 -0400

    reorg dirs

--- scripts/regen_images.sh
+++ scripts/regen_images.sh
@@ -1,9 +1,9 @@
 cd /mnt/c/Users/Bill\ Tyros/Documents/GitHub/doom
-cd wads/castle_of_doom/img
+cd wads/003_castle_of_doom/img
 ../../../scripts/drawmaps2.py ../castle_of_doom.wad MAP01 5000
-cd ../../../wads/speed001/img
+cd ../../../wads/001_speed001/img
 ../../../scripts/drawmaps2.py ../speed001.wad MAP01
-cd ../../../wads/speed002/img
+cd ../../../wads/002_speed002/img
 ../../../scripts/drawmaps2.py ../speed002.wad MAP01
-cd ../../../wads/tomb_of_doom/img
-../../../scripts/drawmaps2.py ../tomb_of_doom.wad MAP01
+cd ../../../wads/004_tomb_of_doom/img
+../../../scripts/drawmaps2.py ../MAP01.wad MAP01
