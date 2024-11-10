ddb626a88db288b0be3fdf33dd0af6d0d915a43c
Bill Tyros <me@strategineer.com>
2024-11-10 17:44:36 -0500

    Name midis properly based on which map they're on

--- doom/ripmidis.py
+++ doom/ripmidis.py
@@ -4,13 +4,15 @@ import sys, getopt
 from pathlib import Path
 
 from omg import *
+# import fluidsynth
 
 verbose = False
 
 if len(sys.argv) < 2:
     print("\n    Omgifol script: rip and render all midis from a given wad\n")
     print("    Usage:")
-    print("    ripmidis.py [-v] [-f] source.wad \n")
+    print("    ripmidis.py [-v] [-a] source.wad \n")
+    print("    -a: short for all, when not present midis not properly tied to maps, will not be exported\n")
 else:
     # process optional flags
     try:
@@ -28,10 +30,30 @@ else:
     inwad = WAD()
     inwad.from_file(sourcewad_filepath)
 
-    ## TODO logic
+    from_music_name_to_map_name = {}
+    mapinfo = inwad.data["MAPINFO"].data.decode('ascii')
+    previous_map_name = None
+    for l in mapinfo.split("\n"):
+        if l.startswith("map "):
+            previous_map_name = l[4:].strip()
+        if l.startswith("music "):
+            music_track = l.split(" ")[1].strip()
+            if music_track not in from_music_name_to_map_name:
+                from_music_name_to_map_name[music_track] = previous_map_name
+
     wad_name = Path(sourcewad_filepath).name
     export_path = Path(f"exports/{wad_name}")
     export_path.mkdir(exist_ok=True)
     for m in inwad.music:
-        midi_filepath = Path(export_path, f"{m}.mid")
+        # todo get a different file name here based on the level
+        if m not in from_music_name_to_map_name:
+            # skip weird tracks unless -a arg is passed
+            continue
+        filename = ''.join(x for x in from_music_name_to_map_name[m] if x.isalnum()) if m in from_music_name_to_map_name else f"_{m}"
+        midi_filepath = Path(export_path, f"{filename}.mid")
         inwad.music[m].to_file(str(midi_filepath))
+        #fs = fluidsynth.Synth()
+        #fs.start()
+        #sfid = fs.sfload('C:\ProgramData\soundfonts\default.sf2')
+        #fs.program_select(0, sfid, 0, 0)
+        #break
