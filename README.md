3beb7a32044a2ae2bf91c595c2bc054c7e07f2a9
Bill Tyros <me@strategineer.com>
2024-11-10 18:56:13 -0500

    asht

--- README.md
+++ README.md
@@ -5,5 +5,17 @@
 Generate overhead editor-esque PNGs for maps like so:
 
 ```
-./scripts/drawmaps.py wads/castle_of_doom/castle_of_doom.wad MAP01 4096 PNG
+poetry run python doom/drawmaps.py wads/castle_of_doom/castle_of_doom.wad MAP01 4096 PNG
+```
+
+Rip midi tracks from the given wad like so:
+
+```
+poetry run python doom/ripmidis.py C:\games\doom\maps\aaliens\aaliens_v1_2.wad
+```
+
+... if you've got fluidsynth set up with your soundfont of choice, then appending a supported audio file format like `oga` or `flac` will also export the midis files for easier sharing:
+
+```
+poetry run python doom/ripmidis.py C:\games\doom\maps\aaliens\aaliens_v1_2.wad oga
 ```
--- doom/ripmidis.py
+++ doom/ripmidis.py
@@ -22,7 +22,7 @@ if len(sys.argv) < 1:
 else:
     # process optional flags
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "vaf")
+        opts, args = getopt.getopt(sys.argv[1:], "vdaf")
         for o, a in opts:
             if o == "-v":
                 verbose = True
@@ -45,33 +45,47 @@ else:
     mapinfo = inwad.data["MAPINFO"].data.decode('ascii')
     previous_map_name = None
     for l in mapinfo.split("\n"):
-        if l.startswith("map "):
+        if verbose:
+            print(l)
+        if l.strip(r" {").startswith("map "):
             previous_map_name = l[4:].strip()
-        if l.startswith("music "):
-            music_track = l.split(" ")[1].strip()
+            print(previous_map_name)
+        if l.strip().startswith("music ") or l.strip().startswith("music ="):
+            music_track = l.split("=")[1].strip() if "=" in l else l.split(" ")[1].strip()
+            print(music_track)
             if music_track not in from_music_name_to_map_name:
-                from_music_name_to_map_name[music_track] = previous_map_name
+                from_music_name_to_map_name[music_track.strip("\"")] = previous_map_name
+    
+    if verbose:
+        print(from_music_name_to_map_name)
 
     wad_name = Path(sourcewad_filepath).stem
+    filename_prefix = f"{wad_name}_"
     extraction_path = Path(f"exports/")
     extraction_path.mkdir(exist_ok=True)
+    n = 0
     if verbose:
         print(f"Extracting midis from {wad_name} to {extraction_path}...")
     for m in inwad.music:
+        # todo these files aren't always midis, I should detect that somehow
         if not all_midis and m not in from_music_name_to_map_name:
             # skip weird tracks unless -a arg is passed
             continue
         filename = ''.join(x for x in from_music_name_to_map_name[m] if x.isalnum()) if m in from_music_name_to_map_name else f"_{m}"
-        midi_filepath = Path(extraction_path, f"{wad_name}_{filename}.mid")
+        midi_filepath = Path(extraction_path, f"{filename_prefix}{filename}.mid")
         if not midi_filepath.exists() or force:
             inwad.music[m].to_file(str(midi_filepath))
+            n += 1
+
+    if verbose:
+        print(f"Exported {n} music tracks")
 
     if len(args) == 1:
         sys.exit()
     exported_extension = f".{args[1]}"
     if verbose:
         print(f"Rendering midis to {extraction_path}...")
-    for f in extraction_path.glob("*.mid"):
+    for f in extraction_path.glob(f"{filename_prefix}*.mid"):
         exported_filename = f.name.replace(".mid", exported_extension)
         exported_filepath = extraction_path.joinpath(exported_filename)
         if not exported_filepath.exists() or force:
